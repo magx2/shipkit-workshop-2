519c9f4236c9fb2627c0e90e4ec4ffdeb0aaa056
Mieszko Wasniowski <mieszko.wasniowski@gmail.com>
2019-02-18 13:24:38 +0100

    Adding junit

--- src/test/java/com/example/shipkitworkshop/MathUtilsTest2.java
+++ src/test/java/com/example/shipkitworkshop/MathUtilsTest2.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class MathUtilsTest {
+public class MathUtilsTest2 {
 
     @Test
     public void shouldCreateTriangle() {
@@ -16,6 +16,11 @@ public class MathUtilsTest {
     @Test
     public void shouldNotCreateTriangle() {
         assertFalse(MathUtils.isTriangle(3, 4, 10));
+    }
+
+    @Test
+    public void shouldVerifyTriangle() {
+    	assertTrue(MathUtils.isTriangle(5, 5, 5));
     }
 
 }
